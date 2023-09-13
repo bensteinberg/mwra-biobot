0c7ecc393a71456226eb294a6d2ebd252055a91b
Ben Steinberg <ben@potswift.org>
2023-09-13 08:14:54 -0400

    Corrections for lintr

--- plot.R
+++ plot.R
@@ -1,15 +1,14 @@
 source("tidy.R")
 
 ggplot(
-    data = tidied %>%
-        filter(Sample.Date>"2023-01-01") %>%
-        filter(Sample.Date<"2023-09-15"),
-    aes(x = Sample.Date, y = Copies.mL)
+  data = tidied %>%
+    filter(Sample.Date > "2023-01-01") %>%
+    filter(Sample.Date < "2023-09-15"),
+  aes(x = Sample.Date, y = Copies.mL)
 ) +
-    geom_point() +
-    geom_smooth() +
-    facet_wrap(~ Region) +
-    ggtitle('MWRA Biobot COVID-19 RNA copies/mL, 2023')
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~ Region) +
+  ggtitle("MWRA Biobot COVID-19 RNA copies/mL, 2023")
 
-ggsave('mwra-biobot-2023-copies-ml.png')
-
+ggsave("mwra-biobot-2023-copies-ml.png")
