2a08808469a3c1187ca32976c4d76989847d088c
Ben Steinberg <ben@potswift.org>
2023-12-28 10:09:08 -0500

    Prepare for new year

--- plot.R
+++ plot.R
@@ -10,6 +10,6 @@ ggplot(
   geom_smooth() +
   scale_x_date(guide = guide_axis(angle = 45)) +
   facet_wrap(~ Region) +
-  ggtitle("MWRA Biobot COVID-19 RNA copies/mL, 2023")
+  ggtitle("MWRA Biobot COVID-19 RNA copies/mL, 2023-")
 
 ggsave("mwra-biobot-2023-copies-ml.png")
