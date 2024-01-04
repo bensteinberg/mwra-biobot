013f74e4441e3725fcb2b55d3ff9c5863145cb6f
Ben Steinberg <ben@potswift.org>
2024-01-04 16:07:39 -0500

    Start graph in November 2022

--- plot.R
+++ plot.R
@@ -3,13 +3,13 @@ source("tidy.R")
 ggplot(
   data = tidied %>%
     drop_na(Copies.mL) %>%
-    filter(Sample.Date > "2023-01-01"),
+    filter(Sample.Date > "2022-11-01"),
   aes(x = Sample.Date, y = Copies.mL)
 ) +
   geom_point() +
   geom_smooth() +
   scale_x_date(guide = guide_axis(angle = 45)) +
   facet_wrap(~ Region) +
-  ggtitle("MWRA Biobot COVID-19 RNA copies/mL, 2023-")
+  ggtitle("MWRA Biobot COVID-19 RNA copies/mL, November 2022-")
 
 ggsave("mwra-biobot-2023-copies-ml.png")
