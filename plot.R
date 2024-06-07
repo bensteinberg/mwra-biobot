644c86c6d426e83154b517b9b158b4bc45f16337
Ben Steinberg <ben@potswift.org>
2024-06-07 06:04:11 -0400

    Make smoothing wigglier

--- plot.R
+++ plot.R
@@ -7,7 +7,7 @@ ggplot(
   aes(x = Sample.Date, y = Copies.mL)
 ) +
   geom_point() +
-  geom_smooth() +
+  geom_smooth(method = "loess", span = 0.3) +
   scale_x_date(guide = guide_axis(angle = 45)) +
   facet_wrap(~ Region) +
   ggtitle("MWRA Biobot COVID-19 RNA copies/mL, November 2022-")
